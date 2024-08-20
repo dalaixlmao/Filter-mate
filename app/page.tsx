ee26312650d920ce2ea236ff7e0edadbdf2818b8
Anubhav <anubhav_a@me.iitr.ac.in>
2024-08-21 01:57:11 +0530

    Added error warning

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Alert from "@/components/Alert";
 import { RootState } from "@/store/store";
 import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,6 +13,8 @@ export default function Home() {
 
   return (
     <div className="h-screen w-screen flex flex-col items-center">
+      <div className="absolute top-10 bg-red-100/10 backdrop-blur-lg px-4 py-2 border-red-500 border-2 rounded-lg flex flex-row">
+      <Alert /> Assignment in progress !</div>
       <div className="md:4/5 lg:w-3/5 bg-white/10 h-full">
         <div className="h-[10%] flex flex-col items-center">
           <SearchBar />
